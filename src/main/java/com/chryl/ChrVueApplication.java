1ec427e96e2d1102f77097c22b83bf56b5f086fc
chryl <cyl5yy412@gmail.com>
2019-12-07 18:00:34 +0800

    init

--- src/main/java/com/chryl/ChrVueApplication.java
+++ src/main/java/com/chryl/ChrVueApplication.java
@@ -3,13 +3,14 @@ package com.chryl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.chryl.mapper")
 public class ChrVueApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChrVueApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChrVueApplication.class, args);
+    }
 
 }
